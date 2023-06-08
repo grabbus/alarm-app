147d417d40e5cdc6fa3dea482a5720650a2f6525
grabbus <mail@michaelgrabinger.com>
2023-06-09 01:55:33 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import path from 'path'
 
 export default defineConfig({
     server: {
-        host: 'localhost',
+        hmr: {
+            host: 'localhost',
+        },
     },
     plugins: [
         laravel({
