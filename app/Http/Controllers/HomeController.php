d4370ebdf2d2e859aba0426772a9b2a51d64e3c0
grabbus <mail@michaelgrabinger.com>
2023-06-09 07:46:05 +0200

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,6 +21,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return redirect()->route('welcome');
+        return redirect()->route('control-center.index');
     }
 }
