60e815c8d7e895140dd68cbbeebbf7ac07726241
grabbus <41126964+grabbus@users.noreply.github.com>
2024-04-17 13:45:29 +0200

    Update alarm-monitor.blade.php
    added debug variable

--- resources/views/alarm-monitor.blade.php
+++ resources/views/alarm-monitor.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <div class="container-fluid">
         {{ date('Y-m-d H:i:s') }}
+        {{ $call }}
         <livewire:alarm-monitor :vehicles="$vehicles" :calls="$calls"/>
     </div>
 @stop
